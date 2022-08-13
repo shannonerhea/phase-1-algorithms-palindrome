288af34627fc396b5d90105e5d622252c5f48e31
shannone <shannone426@gmail.com>
2022-08-13 07:34:24 -0400

    palindrome complete

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
+/* MDN psges i used
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 */
 
 /*
-  Add written explanation of your solution here
+im going to split the word on a space, 
+reverse it 
+and then join it again
+return the reversed word 
 */
+function isPalindrome(word) {
+  let reversed = word.split("").reverse().join("")
+  return word === reversed
+
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
