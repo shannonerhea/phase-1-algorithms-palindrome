785b721313c43fe0fe6d5cb1718a2e718ede8ee3
shannone <shannone426@gmail.com>
2022-08-17 07:46:17 -0400

    wtf forgot everything i knew

--- index.js
+++ index.js
@@ -10,22 +10,93 @@ reverse it
 and then join it again
 return the reversed word 
 */
-function isPalindrome(word) {
-  let reversed = word.split("").reverse().join("")
-  return word === reversed
 
-}
+
+
+// const checkPalindrome = string => {
+//   let reversed = ''
+//   let newString = string.toLowerCase()
+//   for (let i = newString.length -1 ; i >= 0 ; i--) {
+//     const currentLetter = newString[i];
+//     reversed += currentLetter;
+//   }
+  
+//   return reversed === newString ? true : false
+  
+// }
+// function isPalindrome(word) {
+//   if (word.length <= 1) {
+//     return true
+//   } else {
+//     if(word[0] === word[word.length - 1]) {
+//       isPalindrome(word.slice(1, -1))
+//     } else {
+//       return false
+//     }
+//   }
+// }
+// function isPalindrome(str)
+// {
+//      return str === str.split('').reverse().join('')
+// }
+// function isPalindrome(word) {
+//   reversed = "";
+//   i = 0
+//   while i < word.length;
+//    reversed = word[i] + reversed
+//    i += 1
+//    if (word === reversed) {
+//     return true;
+//    } else {
+//     return false
+//    };
+// };
+
+
+// function isPalindrome(word) {
+//   reversed = "";
+//   for (let i = 0; i < word.length; i++) {
+//     reversed = word[i] + reversed
+//     if (word === reversed) {
+//       return true;
+//     } else {
+//       return false
+//     };
+//   };
+// };
+// const isPalindrome = (word) => {
+//   let reversed = word.split("").reverse().join("")
+//   return reversed
+
+//  }
+
+// // You can run `node index.js` to view these console logs
+// //if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+  // console.log("=>", isPalindrome("racecar"));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log("=>", isPalindrome("robot"));
+
+
+
+// function isPalindrome(word) {
+//   let reversed = word.split("").reverse().join("")
+//   return word === reversed
+
+// }
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
